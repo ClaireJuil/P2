5e89027e3bc8b165984cac2ef70b57cb0069ddbf
Claire JUILLARD <cjuillard@NX-DOMAIN.LAN>
2023-07-17 16:26:29 +0200

    demo-client-dam parse de la réponse de keycloak

--- relying-party/src/main/java/fr/ans/psc/config/SecurityConfig.java
+++ relying-party/src/main/java/fr/ans/psc/config/SecurityConfig.java
@@ -2,19 +2,11 @@ package fr.ans.psc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
