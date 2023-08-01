84ae7a4b9992f2af65c2d1a8fb05504ef434e46c
Claire JUILLARD <cjuillard@NX-DOMAIN.LAN>
2023-08-01 12:01:46 +0200

    ajout crls pour getDAMs (dont form). Pages html KO

--- demo-client-dam/src/main/java/fr/ans/psc/client/democlientdam/controllers/GetUserDamsController.java
+++ demo-client-dam/src/main/java/fr/ans/psc/client/democlientdam/controllers/GetUserDamsController.java
@@ -14,13 +14,12 @@ public class GetUserDamsController {
 	 @Autowired
     private ApiCalls api;
 
-	@GetMapping("/admin/api")
-	public String getFormDam(Model model) {
+	@GetMapping("/admin/api/dams")
+	public String getUserDams(Model model) {
 
-		// TODO recup du token d'API et affichage
-		// appel à l'API avec le jeton d'API
-	//	String tmp = api.getMyDams();
+		// TODO 
 		String damResponse ="{\"nationalId\":\"899700245667\",\"dams\":[{\"identifiantLieuDeTravail\":\"99700245667008\",\"typeIdentifiant\":\"Id Cabinet RPPS / N° de registre\",\"codeTypeIdentifiant\":\"6\",\"raisonSociale\":\"CABINET M DOC0024566\",\"modeExercice\":\"Libéral\",\"codeModeExercice\":\"0\",\"numActivite\":\"2102887019\",\"numAssuranceMaladie\":\"001055664\",\"dateDebutValidite\":\"26-06-2020\",\"dateFinValidite\":\"26-06-2023\",\"specialite\":\"Médecine générale\",\"codeSpecialite\":\"01\",\"conventionnement\":\"Conventionné\",\"codeConventionnel\":\"1\",\"indicateurFacturation\":\"Libellé indicateur facturation 2\",\"codeIndicateurFacturation\":\"2\",\"zoneIK\":\"Libellé Code Indemnités kilométriques 1\",\"codeZoneIK\":\"1\",\"zoneTarifaire\":\"Zone B\",\"codeZoneTarifaire\":\"24\",\"agrement1\":\"code non trouvé dans la nomenclature\",\"codeAgrement1\":\"00\",\"agrement2\":\"code non trouvé dans la nomenclature\",\"codeAgrement2\":\"00\",\"agrement3\":\"code non trouvé dans la nomenclature\",\"codeAgrement3\":\"00\",\"habilitationFse\":\"001\",\"habilitationLot\":\"001\"}]}";
-		return "form-dam";
+		model.addAttribute("dams",damResponse);
+		return "display-dam";
 	}
 }
